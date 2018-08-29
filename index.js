2b41ccde5fa53ea8d6376d167694d16e0a38b4d8
LN2410 <naphakadelihle@gmail.com>
2018-08-29 16:44:12 +0200

    l

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ let express = require('express');
 let Greetings = require('./greetings');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-
+const flash = require('express-flash');
+const session = require('express-session');
+// const express = require('express');
 const app = express();
+
+//let app = express();
 
 let greetPeople = Greetings();
 
@@ -20,33 +24,41 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   let count = greetPeople.greetCounter();
-  res.render('greeting', {count});
+  res.render('greeting', {
+    count
+  });
 });
 
-app.post('/greet', function (req, res) {
+app.post('/greet', function(req, res) {
   let name = req.body.name;
   let language = req.body.language;
   let greetName = greetPeople.greet(language, name);
   let count = greetPeople.greetCounter();
 
-  res.render('greeting', {greetName,count});
+  res.render('greeting', {
+    greetName,
+    count
+  });
 });
 
-app.get('/greet/:name/:language', function (req, res) {
+app.get('/greet/:name/:language', function(req, res) {
   let name = req.params.name;
   let language = req.params.language;
   let greetName = greetPeople.greet(language, name);
   let count = greetPeople.greetCounter();
   console.log(req.params);
 
-  res.render('greeting', {greetName, count});
+  res.render('greeting', {
+    greetName,
+    count
+  });
   // res.redirect('/');
 });
 
 const PORT = process.env.PORT || 3015;
 
-app.listen(PORT, function(){
-	console.log("app started at port:", PORT)
+app.listen(PORT, function() {
+  console.log("app started at port:", PORT)
 });
