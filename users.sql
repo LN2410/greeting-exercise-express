1974a6f4fd10d995a8ec39920c4c470dff79269d
LN2410 <naphakadelihle@gmail.com>
2018-09-05 10:27:17 +0200

    j

--- users.sql
+++ users.sql
@@ -1,10 +1,10 @@
--- 
--- create table greetings(
--- 	id serial not null primary key,
---     greeted_names text not null
--- 	);
---
--- create table countGreet(
---     count int not null,
---     foreign key (count) references greetings(id)
--- );
+
+create table greetings(
+	id serial not null primary key,
+    greeted_names text not null
+	);
+
+create table countGreet(
+    count int not null,
+    foreign key (count) references greetings(id)
+);
